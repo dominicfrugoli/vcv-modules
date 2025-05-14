c776c36df0b6d2379e53cfe0d26a620c93558fdd
dominicfrugoli <dominicfrugoli@gmail.com>
2025-05-14 01:35:38 -0500

    Implemented Oscillators

--- src/planky.cpp
+++ src/planky.cpp
@@ -1,7 +1,8 @@
 #include "plugin.hpp"
 
 
-struct Planky : Module {
+struct Planky : Module
+{
 	enum ParamId {
 		FREQ_PARAM,
 		OSC1_PARAM,
@@ -26,53 +27,54 @@ struct Planky : Module {
 		LIGHTS_LEN
 	};
 
-	Planky() {
+	Planky()
+	{
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
-		configParam(FREQ_PARAM, 0.f, 1.f, 0.f, "Master Frequency");
-		configParam(OSC1_PARAM, 0.f, 1.f, 0.f, "Oscillator 1 Offset");
-		configParam(OSC2_PARAM, 0.f, 1.f, 0.f, "Oscillator 2 Offset");
-		configParam(OSC3_PARAM, 0.f, 1.f, 0.f, "Oscillator 3 Offset");
+		configParam(FREQ_PARAM, 20.0f, 3000.0f, 440.0f, "Master Frequency");
+		configParam(OSC1_PARAM, -12.0f, 12.0f, 0.0f, "Oscillator 1 Offset");
+		configParam(OSC2_PARAM, -12.0f, 12.0f, 0.0f, "Oscillator 2 Offset");
+		configParam(OSC3_PARAM, -12.0f, 12.0f, 0.0f, "Oscillator 3 Offset");
 		configInput(FREQ_INPUT, "Master Frequency CV");
-		configOutput(OSC1_OUTPUT, "Oscillator 1 Output");
-		configOutput(OSC2_OUTPUT, "Oscillator 2 Output");
-		configOutput(OSC3_OUTPUT, "Oscillator 3 Output");
+		configOutput(OSC1_OUTPUT, "Oscillator 1");
+		configOutput(OSC2_OUTPUT, "Oscillator 2");
+		configOutput(OSC3_OUTPUT, "Oscillator 3");
 	}
 
+	// Global variables
 	float oscPhases[3] = {0.0f, 0.0f, 0.0f};
 	float sines[3] = {0.0f, 0.0f, 0.0f};
 
+	// Audio Callback
 	void process(const ProcessArgs& args) override
 	{
-		// Compute the frequency from the pitch parameter and input
-		float pitch = params[FREQ_PARAM].getValue();
-		pitch += inputs[FREQ_INPUT].getVoltage();
-		// The default frequency is C4 = 261.6256f
-		float freq = dsp::FREQ_C4 * std::pow(2.f, pitch);
 
-		
-		
-		// Audio signals are typically +/-5V
-		// https://vcvrack.com/manual/VoltageStandards
-		//outputs[SINE_OUTPUT].setVoltage(5.f * sine);
+		// Get the frequency from Master Frequency knob
+		float freq = params[FREQ_PARAM].getValue();
+		//freq +=  220 * std::pow(2.0f, inputs[FREQ_INPUT].getVoltage()); // I don't know how to do the scaling on this
+
 		for(int i = 0; i < 3; i++)
 		{
-			// Accumulate the phase
-			oscPhases[i] += freq * args.sampleTime;
-			if (oscPhases[i] >= 1.f)
-				oscPhases[i] -= 1.f;
+			// Calculate the frequency for each oscillator based on the offset
+			float oscFreq = freq * std::pow(2.f, params[i+1].getValue() / 12.f); // WHY IS OSCILLATOR 3 GETTING A DIFFERENT VALUE?
 
+			// Accumulate the phase
+			oscPhases[i] += oscFreq * args.sampleTime;
+			if (oscPhases[i] >= 1.f)
+			{
+				oscPhases[i] -= 1.f;
+			}
 			// Compute the sine output
 			sines[i] = std::sin(2.f * M_PI * oscPhases[i]);
-
-
 			outputs[i].setVoltage(5.f * sines[i]);
 		}
 	}
 };
 
 
-struct PlankyWidget : ModuleWidget {
-	PlankyWidget(Planky* module) {
+struct PlankyWidget : ModuleWidget
+{
+	PlankyWidget(Planky* module)
+	{
 		setModule(module);
 		setPanel(createPanel(asset::plugin(pluginInstance, "res/Planky.svg")));
 
